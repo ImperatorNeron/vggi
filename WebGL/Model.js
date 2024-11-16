0e13dfed947d64b82e03e89faf32684757d44bfa
ImperatorNeron <katebarspk@gmail.com>
2024-11-16 16:45:02 +0200

    🚀 Fill the figure with triangles

--- WebGL/Model.js
+++ WebGL/Model.js
@@ -5,20 +5,34 @@ function Model(name) {
     this.countsU = [];
     this.countsV = [];
 
-    this.BufferDataHelper = function (lines, counts, iVertexBuffer) {
-        for (let line of lines) {
-            let flatVertices = line.flat();
+    this.BufferDataHelper = function (lines, counts) {
+        // Just for reset all vertices for right direction
+        let bigStrip = []
+        for (let i = 0; i < lines.length - 1; i++) {
+            let strip = []
+            for (let j = 0; j < lines[i].length - 1; j++) {
+                strip.push(lines[i][j])
+                strip.push(lines[i + 1][j])
+                strip.push(lines[i + 1][j + 1])
+                strip.push(lines[i][j])
+                strip.push(lines[i + 1][j + 1])
+                strip.push(lines[i][j + 1])
+            }
+            bigStrip.push(strip)
+        }
+
+        for (let strp of bigStrip) {
+            let flatStrip = strp.flat();
             let buffer = gl.createBuffer();
             gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(flatVertices), gl.STREAM_DRAW);
-            iVertexBuffer.push(buffer);
-            counts.push(flatVertices.length / 3);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(flatStrip), gl.STREAM_DRAW);
+            this.iVertexBufferV.push(buffer);
+            counts.push(flatStrip.length / 3);
         }
     }
 
     this.BufferData = function (vertexLists) {
-        this.BufferDataHelper(vertexLists.uVertexList, this.countsU, this.iVertexBufferU)
-        this.BufferDataHelper(vertexLists.vVertexList, this.countsV, this.iVertexBufferV)
+        this.BufferDataHelper(vertexLists.vVertexList, this.countsV)
     }
 
     this.DrawHelper = function (iVertexBuffer, counts) {
@@ -26,12 +40,11 @@ function Model(name) {
             gl.bindBuffer(gl.ARRAY_BUFFER, iVertexBuffer[i]);
             gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
             gl.enableVertexAttribArray(shProgram.iAttribVertex);
-            gl.drawArrays(gl.LINE_STRIP, 0, counts[i]);
+            gl.drawArrays(gl.TRIANGLES, 0, counts[i]);
         }
     }
 
     this.Draw = function () {
-        this.DrawHelper(this.iVertexBufferU, this.countsU)
         this.DrawHelper(this.iVertexBufferV, this.countsV)
     }
 }
